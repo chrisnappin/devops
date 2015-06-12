717648ce0eb9dc7417c9bd9fbe84f93c21d282bb
chrisnappin <chris@nappin.com>
2015-06-12 10:45:34 +0100

    added comments

--- vagrant-linux-puppet/provision/modules/apache/manifests/init.pp
+++ vagrant-linux-puppet/provision/modules/apache/manifests/init.pp
@@ -1,15 +1,18 @@
 class apache {
 
+  # install the apache2 package
   package {"apache2":
     ensure => present
   }
   
+  # set the docroot to a symlink back to the src directory 
   file { '/var/www/src':
     ensure => 'link',
     target => '/vagrant/src',
     require => Package['apache2']
   }
   
+  # install the 000-default.conf file
   file { '/etc/apache2/sites-available/000-default.conf':
     source => 'puppet:///modules/apache/000-default.conf',
     owner => 'root',
@@ -17,10 +20,10 @@ class apache {
     require => Package['apache2']
   }
   
+  # start the service
   service { "apache2":
     require => Package["apache2"],
     subscribe => [File['/etc/apache2/sites-available/000-default.conf']]
   }
-  
   
 }  
